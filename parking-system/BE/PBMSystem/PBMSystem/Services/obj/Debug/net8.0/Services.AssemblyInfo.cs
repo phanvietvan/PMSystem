62fd2a89e48ac89fdef4f4e4eccc513ee6bf35f8
phanvietvan <vietvanphan430@gmail.com>
2026-05-21 23:40:47 +0700

    feat: auto-assign Admin role to vietvanphan04@gmail.com on Google Sign-in

--- parking-system/BE/PBMSystem/PBMSystem/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ parking-system/BE/PBMSystem/PBMSystem/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273fa724c27088e3a7dce0efbb5bf6fed81c4cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba72f8b04ec0955e99291690f593706c179615d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
