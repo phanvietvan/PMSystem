742b0524d51b264b510eb600ff8b13deb4ccc4e3
nhatduy111003 <phamnhatduy08@gmail.com>
2026-05-23 18:04:45 +0700

    Defind Enum Status

---
+++ parking-system/BE/PBMSystem/PBMSystem/Repositories/Enums/IncidentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Enums
+{
+    public enum IncidentType
+    {
+        LOST_TICKET = 1,
+        WRONG_SLOT = 2,
+        UNPAID = 3,
+        OVERSTAY = 4
+    }
+}
---
+++ parking-system/BE/PBMSystem/PBMSystem/Repositories/Enums/ParkingSlotStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Enums
+{
+    public enum ParkingSlotStatus
+    {
+        AVAILABLE = 1,
+        OCCUPIED = 2,
+        RESERVED = 3,
+        MAINTENANCE = 4,
+        LOCKED = 5
+    }
+}
