c4789dec644de60b436839bcbfd97644630c90e7
nhatduy111003 <phamnhatduy08@gmail.com>
2026-05-23 18:09:18 +0700

    Add 2 Entities ParkingIncident, Parking

---
+++ parking-system/BE/PBMSystem/PBMSystem/Repositories/Entities/ParkingSlot.cs
@@ -0,0 +1,25 @@
+﻿using Repositories.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Entities
+{
+    public class ParkingSlot : BaseEntity
+    {
+        public string SlotCode { get; set; } = string.Empty;
+
+        public ParkingSlotStatus Status { get; set; }
+            = ParkingSlotStatus.AVAILABLE;
+
+        public string? Floor { get; set; }
+
+        public string? Zone { get; set; }
+
+        public string? VehicleType { get; set; }
+
+        public string? QrCode { get; set; }
+    }
+}
