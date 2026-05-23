fc835b215a1470d6ade84d641f566cb68e875d1c
nhatduy111003 <phamnhatduy08@gmail.com>
2026-05-23 18:05:59 +0700

    Session API, active, end,

---
+++ parking-system/BE/PBMSystem/PBMSystem/Services/Interfaces/ISessionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ISessionService
+    {
+        Task<object> GetActiveAsync(Guid userId);
+
+        Task<object> EndAsync(Guid id);
+
+        Task<object> GetFeeAsync(Guid id);
+    }
+}
