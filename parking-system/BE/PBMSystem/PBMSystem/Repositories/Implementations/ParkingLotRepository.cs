56869d0e1582cd14f1caa29f6dd134018078fc4d
nhatduy111003 <phamnhatduy08@gmail.com>
2026-06-25 16:24:33 +0700

    Add ParkingLot Repository and Service and Sign in Program.cs

---
+++ parking-system/BE/PBMSystem/PBMSystem/Repositories/Implementations/ParkingLotRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Entities;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Implementations
+{
+    public class ParkingLotRepository : IParkingLotRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ParkingLotRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ParkingLot>> GetAllAsync()
+        {
+            return await _context.ParkingLots
+                .OrderBy(x => x.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<ParkingLot?> GetByIdAsync(Guid id)
+        {
+            return await _context.ParkingLots.FindAsync(id);
+        }
+
+        public async Task AddAsync(ParkingLot parkingLot)
+        {
+            await _context.ParkingLots.AddAsync(parkingLot);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(ParkingLot parkingLot)
+        {
+            _context.ParkingLots.Update(parkingLot);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
